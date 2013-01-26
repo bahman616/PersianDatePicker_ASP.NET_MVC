656728fa830d096275c1bf6fb28e474076dcb01d
Bahman Nikkhahan <bahman616@gmail.com>
2013-01-26 18:02:13 +0330

    Initial Upload

---
+++ PersianDatePicker-MVC/Controllers/StudentController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersianDatePicker_MVC.Models;
+
+namespace PersianDatePicker_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StuEntities db = new StuEntities();
+
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        //
+        // GET: /Student/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(students);
+        }
+
+        //
+        // GET: /Student/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(students).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(students);
+        }
+
+        //
+        // GET: /Student/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Students students = db.Students.Find(id);
+            db.Students.Remove(students);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PersianDatePicker-MVC/PersianDateExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersianDatePicker_MVC
+{
+    public static class PersianDateExtension
+    {
+        public static string GetPersianDateString(DateTime? date)
+        {
+            string PersianDate = "";
+            if (!date.HasValue)
+            {
+                return PersianDate;
+            }
+            System.Globalization.PersianCalendar p = new System.Globalization.PersianCalendar();
+            int Year = p.GetYear(date.Value);
+            int Month = p.GetMonth(date.Value);
+            int Day = p.GetDayOfMonth(date.Value);
+            string MonthName = "فروردین";
+            switch (Month)
+            {
+                case 1: { MonthName = "فروردین"; break; }
+                case 2: { MonthName = "اردیبهشت"; break; }
+                case 3: { MonthName = "خرداد"; break; }
+                case 4: { MonthName = "تیر"; break; }
+                case 5: { MonthName = "مرداد"; break; }
+                case 6: { MonthName = "شهریور"; break; }
+                case 7: { MonthName = "مهر"; break; }
+                case 8: { MonthName = "آبان"; break; }
+                case 9: { MonthName = "آذر"; break; }
+                case 10: { MonthName = "دی"; break; }
+                case 11: { MonthName = "بهمن"; break; }
+                case 12: { MonthName = "اسفند"; break; }
+                default: { MonthName = "فروردین"; break; }
+            }
+            PersianDate = Day.ToString() + " " + MonthName + " " + Year.ToString();
+            return PersianDate;
+        }
+        public static string GetPersianDateStringSlashed(DateTime? date)
+        {
+            string PersianDate = "";
+            if (!date.HasValue)
+            {
+                return PersianDate;
+            }
+            System.Globalization.PersianCalendar p = new System.Globalization.PersianCalendar();
+            int Year = p.GetYear(date.Value);
+            int Month = p.GetMonth(date.Value);
+            int Day = p.GetDayOfMonth(date.Value);
+
+            PersianDate = Day.ToString() + "/" + Month.ToString() + "/" + Year.ToString();
+            return PersianDate;
+        }
+
+    }
+}
